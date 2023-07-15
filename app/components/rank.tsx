7e3b931fe788e95629e4f4daf04a0fd81226eb14
chenyoufu <chenyoufu@jiancent.com>
2023-07-15 15:04:58 +0800

    feat: 优化排行榜

--- app/components/rank.tsx
+++ app/components/rank.tsx
@@ -1,18 +1,28 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./rank.module.scss";
 import useUser from "../hooks/useUser";
 import { IconButton } from "./button";
+import CloseIcon from "../icons/close.svg";
+import { Path } from "../constant";
 
 const Rank = () => {
   const { getAllUser } = useUser();
+  const navigate = useNavigate();
   const [rankType, setRankType] = useState("today"); // today or week
   const [allUser, setAllUser] = useState<
     Array<{ userName: string; todayRank: number; totalRank: number }>
   >([]);
 
+  const goHome = () => navigate(Path.Home);
+
   useEffect(() => {
     getAllUser().then((users) => {
-      setAllUser(users);
+      setAllUser(
+        [...users].sort(
+          (prev: any, cur: any) => cur.todayRank - prev.todayRank,
+        ),
+      );
     });
   }, []);
 
@@ -37,24 +47,32 @@ const Rank = () => {
   };
 
   const todayRankAward: any = {
-    1: '300积分+300￥',
-    2: '200积分+200￥',
-    3: '100积分+100￥',
+    1: "300积分+300￥",
+    2: "200积分+200￥",
+    3: "100积分+100￥",
   };
 
   const weekRankAward: any = {
-    1: '300积分+300￥',
-    2: '200积分+200￥',
-    3: '100积分+100￥',
+    1: "300积分+300￥",
+    2: "200积分+200￥",
+    3: "100积分+100￥",
   };
 
   return (
     <div className={styles["rank"]}>
       <div className={styles["rank-title"]}>
-        <span className="title">排行榜</span>
+        <div className={styles["row-end"]}>
+          <span className="title">排行榜</span>
+          <IconButton
+            text={rankType === "today" ? "切换周榜" : "切换日榜"}
+            onClick={() => changeRank(rankType)}
+          />
+        </div>
         <IconButton
-          text={rankType === "today" ? "切换周榜" : "切换日榜"}
-          onClick={() => changeRank(rankType)}
+          icon={<CloseIcon />}
+          className={"password-eye"}
+          bordered
+          onClick={goHome}
         />
       </div>
       <table>
